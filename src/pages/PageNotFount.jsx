edc0dbaa156ce440d863669ee0cd8d5995db7018
Aryan Aguiar <aguiararyan@gmail.com>
2025-04-15 22:54:34 +0530

    fixed background

--- src/pages/PageNotFount.jsx
+++ src/pages/PageNotFount.jsx
@@ -21,8 +21,6 @@ const PageNotFount = () => {
                 }}
             >
                 {/* Blurred background glow */}
-               
-
                 <Typography
                     variant="h1"
                     sx={{
